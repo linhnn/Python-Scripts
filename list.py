4b5acdb4d17827f551fc7372f35c1d365b855b42
linhnn <linhvt22@gmail.com>
2015-04-22 10:05:09 +0700

    Create list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+a[start:end] # items start through end-1, if you're counting from 1 anyway. 
+
+a[start:]    # items start through the rest of the list
+
+a[:end]      # items from the beginning through end-1 
+
+a[:]         # a copy of the whole list
+
+a[::-1] # Invert string
